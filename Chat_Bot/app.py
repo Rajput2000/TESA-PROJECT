a90fcd9e4fe35a833f14e796d9e6311b9d051757
Rajput2000 <rajigbenga5@gmail.com>
2025-07-10 14:49:08 +0100

    Update app.py

--- Chat_Bot/app.py
+++ Chat_Bot/app.py
@@ -3,8 +3,9 @@ from src.chatbot import ChatBot
 import os 
 
 st.write(os.getcwd())  # Debugging line to check current working directory
-os.chdir("main/Chat_Bot/library")  # Change to the directory where the library file is located
-st.write(os.getcwd())  # Debugging line to check current working directory
+#list folders in current directory
+st.write(os.listdir('.'))  # List files and directories in the current directory
+
 
 """# Bot initialization
 chat_bot = ChatBot()
